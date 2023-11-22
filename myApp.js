627a9afed81551d61330b9b78bc9188a7ce56a65
Arhaan <102715752+arhaanhamid@users.noreply.github.com>
2023-11-22 15:32:04 +0530

    test

--- myApp.js
+++ myApp.js
@@ -1,9 +1,10 @@
-let express = require("express");
-let app = express();
+// let express = require("express");
+// let app = express();
 
-console.log("Hello World");
-app.get("/", function (req, res) {
-  res.send("Hello Express");
-});
+// console.log("Hello World");
 
-module.exports = app;
+// app.get("/", function (req, res) {
+//   res.send("Hello Express");
+// });
+
+// module.exports = app;
