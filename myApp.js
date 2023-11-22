1e24d60ad6de881bd173b16b2585d0fe474a3e17
Arhaan <102715752+arhaanhamid@users.noreply.github.com>
2023-11-22 15:42:22 +0530

    express get commit

--- myApp.js
+++ myApp.js
@@ -1,10 +1,10 @@
-// let express = require("express");
-// let app = express();
+let express = require("express");
+let app = express();
 
-// console.log("Hello World");
+console.log("Hello World");
 
-// app.get("/", function (req, res) {
-//   res.send("Hello Express");
-// });
+app.get("/", function (req, res) {
+  res.send("Hellow Express");
+});
 
-// module.exports = app;
+module.exports = app;
