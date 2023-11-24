f1e2c1586f9c4ab026c04f386b871cc6f97956ee
Arhaan <102715752+arhaanhamid@users.noreply.github.com>
2023-11-24 11:20:07 +0530

    serve json

--- myApp.js
+++ myApp.js
@@ -11,6 +11,10 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/json", function (req, res) {
+  res.json({ message: "Hello json" });
+});
 
 module.exports = app;
