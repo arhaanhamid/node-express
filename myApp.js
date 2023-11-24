e3a832b1d5e545d03ea52f8bcecfadc62e808df3
Arhaan <102715752+arhaanhamid@users.noreply.github.com>
2023-11-24 15:50:41 +0530

    middleware chaining

--- myApp.js
+++ myApp.js
@@ -4,21 +4,26 @@ require("dotenv").config();
 
 console.log("Hello World");
 
+//middleware function using request, response and next()
 app.use(function (req, res, next) {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
 });
 
+//middleware function used for accessing static folder/files
 app.use("/public", express.static(__dirname + "/public"));
 
+//api request on route '/string' using method 'GET' to simply return a simple string
 app.get("/string", function (req, res) {
   res.send("Hello Express");
 });
 
+//api request on route '/' using method 'GET' to return an html page
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+//api request on route '/json' using method 'GET' to return json object depending on env variable
 app.get("/json", function (req, res) {
   let data = { message: "Hello json" };
   process.env.MESSAGE_STYLE === "uppercase"
@@ -26,5 +31,16 @@ app.get("/json", function (req, res) {
     : "";
   res.json(data);
 });
+
+app.get(
+  "/now",
+  function (req, res, next) {
+    req.time = new Date().toString();
+    next();
+  },
+  function (req, res) {
+    res.send(req.time);
+  }
+);
 
 module.exports = app;
