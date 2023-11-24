bfa7cf00ba87a50fc8e68e4e789318105cc0174e
Arhaan <102715752+arhaanhamid@users.noreply.github.com>
2023-11-24 15:15:31 +0530

    root level middleware t3

--- myApp.js
+++ myApp.js
@@ -4,23 +4,23 @@ require("dotenv").config();
 
 console.log("Hello World");
 
-// app.get("/string", function (req, res) {
-//   res.send("Hello Express");
-// });
+app.get("/string", function (req, res) {
+  res.send("Hello Express");
+});
 
-// app.get("/", function (req, res) {
-//   res.sendFile(__dirname + "/views/index.html");
-// });
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 // app.use("/public", express.static(__dirname + "/public"));
 
-// app.get("/json", function (req, res) {
-//   let data = { message: "Hello json" };
-//   process.env.MESSAGE_STYLE === "uppercase"
-//     ? (data.message = data.message.toUpperCase())
-//     : "";
-//   res.json(data);
-// });
+app.get("/json", function (req, res) {
+  let data = { message: "Hello json" };
+  process.env.MESSAGE_STYLE === "uppercase"
+    ? (data.message = data.message.toUpperCase())
+    : "";
+  res.json(data);
+});
 
 app.use(function (req, res, next) {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
